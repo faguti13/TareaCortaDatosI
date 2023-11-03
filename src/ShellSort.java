a7e35c7e95b22c742f921bf03a1156dbaf550ec4
Adrián <adrianmnzalv105@gmail.com>
2023-11-03 15:51:04 -0600

    Se agrega shell e insertion

--- src/ShellSort.java
+++ src/ShellSort.java
@@ -1,5 +1,19 @@
+/**
+ * Benchmark del algoritmo de ordenamiento Shell Sort con n cantidad de elementos random
+ * @author Adrián Muñoz con la ayuda de I.A. de bing
+ */
 public class ShellSort {
-    public static void shellSort(int[] arr){
-
+    public static void shellSort(int[] arr) {
+        int n = arr.length;
+        for (int gap = n / 2; gap > 0; gap /= 2) {
+            for (int i = gap; i < n; i += 1) {
+                int temp = arr[i];
+                int j;
+                for (j = i; j >= gap && arr[j - gap] > temp; j -= gap) {
+                    arr[j] = arr[j - gap];
+                }
+                arr[j] = temp;
+            }
+        }
     }
 }
